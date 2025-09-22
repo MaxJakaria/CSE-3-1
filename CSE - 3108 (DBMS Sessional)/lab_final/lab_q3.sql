1d97e23d0b01a25ed305daf50900240f360bf1c5
Max-Jakaria <maxjakaria824@gmail.com>
2025-09-22 16:16:47 +0600

    update

--- CSE - 3108 (DBMS Sessional)/lab_final/lab_q3.sql
+++ CSE - 3108 (DBMS Sessional)/lab_final/lab_q3.sql
@@ -1,23 +1,25 @@
 USE lab_q2_q3_university_db;
-DELIMITER $$
 
-CREATE PROCEDURE IF NOT EXISTS GetTopStudentsByDepartment(IN dept_id INT)
+DELIMITER//
+
+CREATE Procedure IF NOT EXISTS GetTopStudentsByDepartment (IN dept_id INT)
 BEGIN
-    SELECT s.student_id, s.name, g.subject, g.grade
-    FROM Students s
-    JOIN Departments d ON s.department_id = d.department_id
-    JOIN Grades g ON s.student_id = g.student_id
-    WHERE s.department_id = dept_id
-      AND g.grade = (
-          SELECT MAX(g2.grade)
-          FROM Grades g2
-          JOIN Students s2 ON g2.student_id = s2.student_id
-          WHERE s2.department_id = dept_id
-            AND g2.subject = g.subject
-      )
-    ORDER BY g.subject;
-END $$
 
-DELIMITER ;
+SELECT s.student_id, s.name, g.subject, g.grade
+FROM
+    students AS s
+    JOIN departments AS d ON s.department_id = d.department_id
+    JOIN grades AS g ON s.student_id = g.student_id
+    WHERE d.department_id = dept_id
+    AND (
+      SELECT g2.subject, Max(g2.grade)
+      FROM students AS s2 RIGHT JOIN grades AS g2
+      ON s2.student_id = g2.student_id
+      WHERE s2.department_id = department_id
+      GROUP BY g2.subject
+    );
+ END//
 
-CALL `GetTopStudentsByDepartment`(101);
+DELIMITER;
+
+CALL `GetTopStudentsByDepartment` (101);
